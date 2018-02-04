75171b85dbfd4b90b62ef2008d40f7604202b991
Jason McCoy <visionquest1972@gmail.com>
2018-02-03 21:30:15 -0500

    added starter php files

---
+++ dist/app/includes/footer.php
@@ -0,0 +1,21 @@
+<!-- modernizr -->
+<script src="src/js/libs/modernizr-3.5.0.min.js"></script>
+
+<!-- jQuery -->
+<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+<script>
+window.jQuery || document.write('<script src="src/js/vendor/jquery-3.2.1.min.js"><\/script>')
+</script>
+
+<!-- other JavaScript files needed for project -->
+
+
+<!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+<script>
+    window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
+    ga('create','UA-XXXXX-Y','auto');ga('send','pageview')
+</script>
+<script src="https://www.google-analytics.com/analytics.js" async defer></script>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php require('dist/app/includes/head.php') ?>
+
+<div class="container">
+  <header>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In iusto debitis, amet inventore, iure repellendus distinctio, provident nobis pariatur quia ex tempora. Voluptates sapiente, dolor unde aliquam, perspiciatis delectus earum?</header>
+  <nav><ul>
+    <li>Link 1</li>
+    <li>Link 2</li>
+    <li>Link 3</li>
+  </ul></nav>
+  <main>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi error, delectus aspernatur nostrum vel beatae quidem minus animi alias deserunt architecto ad incidunt unde. Laboriosam quisquam iusto sit tenetur quibusdam?</main>
+  <aside>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, voluptatum. Vero harum laboriosam rem doloribus animi sequi quos iusto at ut sapiente earum, repellendus. Recusandae vero blanditiis exercitationem. Quidem, officiis?</aside>
+  <footer>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi recusandae iste aliquid quasi asperiores eaque rerum, deleniti hic pariatur ullam quam. Reiciendis aliquam rerum ab voluptatem nulla dolorum officia nam!</footer>
+</div>
+
+<?php require('dist/app/includes/footer.php') ?>
